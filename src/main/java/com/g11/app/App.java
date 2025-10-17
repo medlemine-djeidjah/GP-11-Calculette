fc16ba11cf4f87d61fd8215d16e98151a416a3a0
medlemine-djeidjah <djmed3844@gmail.com>
2025-10-17 12:46:43 +0000

    initial commit

---
+++ src/main/java/com/g11/app/App.java
@@ -0,0 +1,13 @@
+package com.g11.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
