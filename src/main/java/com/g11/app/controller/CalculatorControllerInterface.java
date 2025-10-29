ef325a8a631fc07ca13dcd58dca19e010a7a31d1
Moustapha-Cheikh07 <23007@esp.mr>
2025-10-29 17:54:58 +0000

    Add calculator controller interface

---
+++ src/main/java/com/g11/app/controller/CalculatorControllerInterface.java
@@ -0,0 +1,8 @@
+package com.g11.app.controller;
+
+import java.util.List;
+
+public interface CalculatorControllerInterface {
+    void changeAccu(String value);
+    void changeStackData(List<Double> stackData);
+}
